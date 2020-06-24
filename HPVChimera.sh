42994dbff717a9d07bce227731c9bbac0614f88f
root <root@bioinfo.labmed.ki.se>
2020-06-24 22:29:03 +0200

    Updating script

--- HPVChimera.sh
+++ HPVChimera.sh
@@ -1,62 +1,69 @@
 if_fasta_file() {
- file=$1
- extension="${file##*.}"
- if [ ! -f "$file" ] || [ $extension != "fasta" ]; then
-     echo "This is not a .fasta file or the $file does exist."
- fi
+  file=$1
+  extension="${file##*.}"
+  if [ ! -f "$file" ] || [ $extension != "fasta" ]; then
+    echo "HPV Chimera check failed."
+    echo "This is not a .fasta file or the $file does exist."
+    exit $?
+  fi
+}
 
+checkExit()
+{
+ if [ "$?" -ne 0 ]; then
+    set -e
+    echo Command $1 exited with abnormal status
+    echo "HPV Chimera check failed."
+    exit $?;    
+  fi
 }
 
 extract_id_indivial_files() {
-
-cat $fasta_file |sed $'s/[^[:print:]\t]//g'  |awk '{if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}print $0 > filename}'
-echo -e "Fasta file well divided"
-
+  cat $fasta_file |sed $'s/[^[:print:]\t]//g'  |awk '{if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}print $0 > filename}'
+  echo -e "Fasta file well divided"
 }
 
 divide_in_three(){
-echo "-------- Divide in three -----------"
-
-cd $root_folder
-cd workspace
-ls | grep -v fasta| grep -v output | grep -v div| while read filename;do
-    extension="${filename%.*}"
-    is_candidate=$(cat $result_file |grep -w $filename| wc -l) 
-    if [ "$is_candidate" -eq "0" ]; then
-      echo -e "$filename is a candidate"
-      long=$(cat $filename | tail -n +2|sed "/\w+/g" |wc -m)
-      sequence=$(cat $filename | tail -n +2)
-      L1=$(expr $long / 3)
-      L11=$(expr $L1 - 1)
-      L2=$(expr $long \* 2)
-      L2=$(expr $L2 / 3)
-      echo $filename --- $long "----" $L1 "----" $L2
-      var1="${sequence:0:$L1}"
-      var2="${sequence:$L1:$L2-$L1}"
-      var3="${sequence:$L2}"
-      mkdir -p div
-      echo -e ">"$extension"_1">div/"$extension"_1.fasta
-      echo -e $var1>>div/"$extension"_1.fasta
-      echo -e ">"$extension"_2">div/"$extension"_2.fasta
-      echo -e $var2>>div/"$extension"_2.fasta
-      echo -e ">"$extension"_3">div/"$extension"_3.fasta
-      echo -e $var3>>div/"$extension"_3.fasta
-   fi
-done
-
+  echo "-------- Divide in three -----------"
+  cd $root_folder
+  cd workspace
+  ls | grep -v fasta| grep -v output | grep -v div| while read filename;do
+      extension="${filename%.*}"
+      is_candidate=$(cat $result_file |grep -w $filename| wc -l) 
+      if [ "$is_candidate" -eq "0" ]; then
+        echo -e "$filename is a candidate"
+        long=$(cat $filename | tail -n +2|sed "/\w+/g" |wc -m)
+        sequence=$(cat $filename | tail -n +2)
+        L1=$(expr $long / 3)
+        L11=$(expr $L1 - 1)
+        L2=$(expr $long \* 2)
+        L2=$(expr $L2 / 3)
+        echo $filename --- $long "----" $L1 "----" $L2
+        var1="${sequence:0:$L1}"
+        var2="${sequence:$L1:$L2-$L1}"
+        var3="${sequence:$L2}"
+        mkdir -p div
+        echo -e ">"$extension"_1">div/"$extension"_1.fasta
+        echo -e $var1>>div/"$extension"_1.fasta
+        echo -e ">"$extension"_2">div/"$extension"_2.fasta
+        echo -e $var2>>div/"$extension"_2.fasta
+        echo -e ">"$extension"_3">div/"$extension"_3.fasta
+        echo -e $var3>>div/"$extension"_3.fasta
+     fi
+  done
 }
 
 perc_identity_90()
 {
- ls | grep fasta | while read filename;do
-   mkdir -p output
-   blastn -db $hpv_database -query "$filename" -perc_identity 90 -out output/"$filename.out"
-
- done
+  ls | grep fasta | while read filename;do
+    mkdir -p output
+    blastn -db $hpv_database -query "$filename" -perc_identity 90 -out output/"$filename.out"
+    checkExit
+  done
 }
 
 blast_85(){
- echo -e "------ blast 85 ---------"
+  echo -e "------ blast 85 ---------"
   ls | grep -v fasta| while read filename;do
     extension="${filename%.*}"  
     mkdir -p output
@@ -73,15 +80,19 @@ blast_85_div()
     extension="${filename%.*}"
     mkdir -p output
     echo -e "blastn for $filename"
-    blastn -db /tmp/chimera/HPV_COMPLETE_GENOME_REFERENCES.fasta -query $filename -perc_identity 85  -outfmt "6 qseqid sseqid pident qcovs length mismatch gapopen qstart qend sstart send evalue bitscore" -out output/"$extension""_concoverageb.out"
-    done 
+    blastn -db $hpv_database -query $filename -perc_identity 85  -outfmt "6 qseqid sseqid pident qcovs length mismatch gapopen qstart qend sstart send evalue bitscore" -out output/"$extension""_concoverageb.out"
+    checkExit
+  done 
 }
 
 
 get_only_first_line_unique()
 {
   echo -e "------ get only unique line -----"
-  cd output
+  if [ -d "output" ] 
+  then
+    cd output
+  fi
   ls | grep -v fasta| while read filename;do
      mkdir -p unique
      extension="${filename%.*}"
@@ -107,7 +118,7 @@ get_only_first_line_unique()
      fi  
      count=$(cat /tmp/count_txt.log)
      rm -rf /tmp/count_txt.log
-     echo -e "Number of Evalue found" $count
+     echo -e "Number of Bitscore found" $count
      cat $filename | head -$count >unique/"$extension"_unique.out
      echo -e "Unique head for $filename"
   done
@@ -123,7 +134,7 @@ check_coverage()
     if [ -s "$filename" ] 
     then
         value=$(cat $filename | awk -F " " {'print $4'})
-	echo "----------- $extension has some data. Value: $value"
+        echo "----------- $extension has some data. Value: $value"
         echo "$value" "-lt" "$value_to_discriminate"  $filename
         if [ "$value" -lt "$value_to_discriminate" ]; then
           
@@ -132,11 +143,9 @@ check_coverage()
         else
           echo "$extension.fa -> NO Chimera: $value"
         fi
-   
     else
-	echo "$extension.fa -> Chimera: The top hit alignment does not cover >60% of contigs sequence"
+        echo "$extension.fa -> Chimera: The top hit alignment does not cover >60% of contigs sequence"
         echo "$extension.fa -> Chimera: The top hit alignment does not cover >60% of contigs sequence">>$result_file
-        
     fi
   done
 }
@@ -146,7 +155,6 @@ last_check()
   echo -e "--------- Last check ----------"
   cd unique
   list=($(ls | awk -F "_" '{print $1}'|sort -u | uniq))
-
   for i in "${list[@]}";do
     cd $root_folder/workspace/div/output/unique
     echo "---------"
@@ -170,7 +178,6 @@ last_check()
             three_column_1=$(echo $three_column  | awk -F " " '{print $1}')
             three_column_2=$(echo $three_column  | awk -F " " '{print $2}')
             three_column_3=$(echo $three_column  |  awk -F " " '{print $3}')
-
             four_column_1=$(echo $four_column  | awk -F " " '{print $1}')
             four_column_2=$(echo $four_column  | awk -F " " '{print $2}')
             four_column_3=$(echo $four_column  | awk -F " " '{print $3}')
@@ -233,12 +240,8 @@ last_check()
          
          
       fi
-      
-
     fi
   done
-
-
 }
 help()
 { 
@@ -249,7 +252,6 @@ help()
   echo -e "2. Folder to work"
   echo -e "3. HPV database fasta file."
   echo -e "''''''''''''''''''"
-
 }
 header()
 {
@@ -258,31 +260,32 @@ header()
   echo "************************************"
 
 }
-end(){
-
-echo "HPV Chimera Check Finished"
-echo "-------------------------"
-echo "Check result in: $folder_to_start/chimera/HPVchimera_results.txt"
-
+end()
+{
+  echo "HPV Chimera Check Finished"
+  echo "-------------------------"
+  echo "Check result in: $folder_to_start/chimera/HPVchimera_results.txt"
 }
 
 if [ "$#" -ne 3 ]; then
-    echo "Illegal number of parameters"
-    help
-    exit 0
+  echo "Illegal number of parameters"
+  echo "HPV Chimera check failed."
+  help
+  exit 0
 fi
 
 
 
 if [ "$1" == "-h" ] ; then
-    echo "Usage: `basename $0` [-h]"
-    help
-    exit 0
+  echo "Usage: `basename $0` [-h]"
+  help
+  exit 0
 fi
 
 fasta_file=$1
 folder_to_start=$2
 hpv_database=$3
+set -e
 mkdir -p $folder_to_start/chimera
 val_identity_ninety=90.000
 val_identity_five=5.000
